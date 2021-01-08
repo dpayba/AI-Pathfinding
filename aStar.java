49380932859be5bef78ac874a4b3715ca4f98c15
Dane Payba <72733141+dpayba@users.noreply.github.com>
2021-01-07 23:39:28 -1000

    Fixed some issues

--- aStar.java
+++ aStar.java
@@ -4,9 +4,6 @@ import java.util.Arrays;
 
 public class aStar {
 	
-	public static double horizontal = 10;
-	public static double diagonal = 14;
-	
 	public enum Direction {NORTH, SOUTH, EAST, WEST, NORTH_WEST, NORTH_EAST, SOUTH_EAST, SOUTH_WEST};
 	
 	public static boolean isDestination(int row, int col, int xEnd, int yEnd) {
@@ -16,38 +13,62 @@ public class aStar {
 			return false;
 	}
 	
-	public static int calcGValue(int row, int col, int xStart, int xEnd) {
-		int gCost = 0;
-		while ((row != xStart) && (col != xEnd)) {
-			if (row != col) {
-				if (row > col) {
-					row--;
-					gCost += horizontal;
-				}
-				else {
-					col--;
-					gCost += horizontal;
-				}
-			}
-			else {
-				row--;
-				col--;
-				gCost += diagonal;
-			}
-		}
-		return gCost;
+	// distance from coords to start
+	public static int calcGValue(double graph[][], int row, int col, int xStart, int yStart) {
+		double rowSum = 0;
+		double colSum = 0;
 		
+		if (row < xStart) {
+			for (int i = row; i < xStart; i++) 
+				rowSum += graph[i][col];
+		}
+		else {
+			for (int i = xStart; i < row; i++) 
+				rowSum += graph[i][col];
+		}
+		
+		if (col < yStart) {
+			for (int i = col; i < yStart; i++) 
+				colSum += graph[row][i];
+		}
+		else {
+			for (int i = yStart; i < col; i++) 
+				colSum += graph[row][i];
+		}
+		
+		return (int) (10 * Math.sqrt(((rowSum)*(rowSum)+ (colSum)* (colSum))));
 	}
 	
 	// distance from start to end
-	public static int calcHValue(int row, int col, int xEnd, int yEnd) {
-		return (int) (10 * Math.sqrt(((xEnd - row)*(xEnd - row)+ (yEnd - col)* (yEnd - col))));
+	public static int calcHValue(double graph[][], int row, int col, int xEnd, int yEnd) {
+		double rowSum = 0;
+		double colSum = 0;
 		
+		if (row < xEnd) {
+			for (int i = row; i < xEnd; i++) 
+				rowSum += graph[i][col];
+		}
+		else {
+			for (int i = xEnd; i < row; i++) 
+				rowSum += graph[i][col];
+		}
+		
+		if (col < yEnd) {
+			for (int i = col; i < yEnd; i++) 
+				colSum += graph[row][i];
+		}
+		else {
+			for (int i = yEnd; i < col; i++) 
+				colSum += graph[row][i];
+		}
+		
+		return (int) (10 * Math.sqrt(((rowSum)*(rowSum)+ (colSum)* (colSum))));
 	}
 	
 	public static Direction AStar(double graph[][], int xStart, int yStart, int xEnd, int yEnd) {
 		int x = xStart;
 		int y = yStart;
+		int maxHCost = calcHValue(graph, x-1, y, xEnd, yEnd); // NORTH
 		
 		ArrayList<Direction> dir = new ArrayList<Direction>(); 
 		ArrayList<Double> maxCost = new ArrayList<Double>();
@@ -58,36 +79,51 @@ public class aStar {
 		
 		double fCost = 0; 
 		int i = 0;
-		maxCost.add((double) 0);
-		dir.add(Direction.NORTH);
 		while (!isDestination(x, y, xEnd, yEnd)) {
-		
+			maxCost.add((double) 0);
+			dir.add(Direction.NORTH);
 			
 			// NORTH
 			if ((x-1) >= 0) {
 				double north = graph[x-1][y];
-				fCost = north * (calcHValue(x-1, y, xEnd, yEnd) + calcGValue(x-1, y, xStart, yStart));
-				if (fCost > maxCost.get(i)) {
+				if (x == 0) 
+					fCost = 0;
+				else  {
+					
+					fCost = north * (calcHValue(graph, x-1, y, xEnd, yEnd) + calcGValue(graph, x-1, y, xStart, yStart));
+				}
+				if (fCost >= maxCost.get(i) && (calcHValue(graph, x-1, y, xEnd, yEnd) <= maxHCost)) {
+					maxHCost = calcHValue(graph, x-1, y, xEnd, yEnd);
 					maxCost.set(i, fCost);
 					dir.set(i, Direction.NORTH);
 				}
 			}
 			
+			
 			// SOUTH
 			if ((x+1) < lenRows) {
 				double south = graph[x+1][y];
-				fCost = south * (calcHValue(x+1, y, xEnd, yEnd) + calcGValue(x+1, y, xStart, yStart));
-				if (fCost > maxCost.get(i)) {
+				if (x == lenRows)
+					fCost = 0;
+				else
+					fCost = south * (calcHValue(graph, x+1, y, xEnd, yEnd) + calcGValue(graph, x+1, y, xStart, yStart));
+				if (fCost >= maxCost.get(i) && (calcHValue(graph, x+1, y, xEnd, yEnd) <= maxHCost)) {
+					maxHCost = calcHValue(graph, x+1, y, xEnd, yEnd);
 					maxCost.set(i, fCost);
 					dir.set(i, Direction.SOUTH);
 				}
 			}
 			
+			
 			// EAST
 			if ((y+1) < lenCols) {
 				double east = graph[x][y+1];
-				fCost = east * (calcHValue(x, y+1, xEnd, yEnd) + calcGValue(x, y+1, xStart, yStart));
-				if (fCost > maxCost.get(i)) {
+				if (y == lenCols - 1)
+					fCost = 0;
+				else
+					fCost = east * (calcHValue(graph, x, y+1, xEnd, yEnd) + calcGValue(graph, x, y+1, xStart, yStart));
+				if (fCost >= maxCost.get(i) && (calcHValue(graph, x, y+1, xEnd, yEnd) <= maxHCost)) {
+					maxHCost = calcHValue(graph, x, y+1, xEnd, yEnd);
 					maxCost.set(i, fCost);
 					dir.set(i, Direction.EAST);
 				}
@@ -96,8 +132,12 @@ public class aStar {
 			// WEST
 			if ((y-1) >= 0) {
 				double west = graph[x][y-1];
-				fCost = west * (calcHValue(x, y-1, xEnd, yEnd) + calcGValue(x, y-1, xStart, yStart));
-				if (fCost > maxCost.get(i)) {
+				if (y == 0)
+					fCost = 0;
+				else
+					fCost = west * (calcHValue(graph, x, y-1, xEnd, yEnd) + calcGValue(graph, x, y-1, xStart, yStart));
+				if (fCost >= maxCost.get(i) && (calcHValue(graph, x, y-1, xEnd, yEnd) <= maxHCost)) {
+					maxHCost = calcHValue(graph, x, y-1, xEnd, yEnd);
 					maxCost.set(i, fCost);
 					dir.set(i, Direction.WEST);
 				}
@@ -106,8 +146,12 @@ public class aStar {
 			// NORTH-EAST
 			if (((x-1) >= 0) && (y+1 < lenCols)) {
 				double northEast = graph[x-1][y+1];
-				fCost = northEast * (calcHValue(x-1, y+1, xEnd, yEnd) + calcGValue(x-1, y+1, xStart, yStart));
-				if (fCost > maxCost.get(i)) {
+				if (x == 0 || y == lenCols - 1)
+					fCost = 0;
+				else
+					fCost = northEast * (calcHValue(graph, x-1, y+1, xEnd, yEnd) + calcGValue(graph, x-1, y+1, xStart, yStart));
+				if (fCost >= maxCost.get(i) && (calcHValue(graph, x-1, y+1, xEnd, yEnd) <= maxHCost)) {
+					maxHCost = calcHValue(graph, x-1, y+1, xEnd, yEnd);
 					maxCost.set(i, fCost);
 					dir.set(i, Direction.NORTH_EAST);
 				}
@@ -116,8 +160,12 @@ public class aStar {
 			// NORTH-WEST
 			if (((x-1) >= 0) && ((y-1) >= 0)) {
 				double northWest = graph[x-1][y-1];
-				fCost = northWest * (calcHValue(x-1, y-1, xEnd, yEnd) + calcGValue(x-1, y-1, xStart, yStart));
-				if (fCost > maxCost.get(i)) {
+				if (x == 0 || y == 0)
+					fCost = 0;
+				else
+					fCost = northWest * (calcHValue(graph, x-1, y-1, xEnd, yEnd) + calcGValue(graph, x-1, y-1, xStart, yStart));
+				if (fCost >= maxCost.get(i) && calcHValue(graph, x-1, y-1, xEnd, yEnd) <= maxHCost) {
+					maxHCost = calcHValue(graph, x-1, y-1, xEnd, yEnd);
 					maxCost.set(i, fCost);
 					dir.set(i, Direction.NORTH_WEST);
 				}
@@ -126,8 +174,12 @@ public class aStar {
 			// SOUTH-EAST
 			if (((x+1) < lenRows) && (y+1 < lenCols)) {
 				double southEast = graph[x+1][y+1];
-				fCost = southEast * (calcHValue(x+1, y+1, xEnd, yEnd) + calcGValue(x+1, y+1, xStart, yStart));
-				if (fCost > maxCost.get(i)) {
+				if (x == lenRows-1 || y == lenCols-1)
+					fCost = 0;
+				else
+					fCost = southEast * (calcHValue(graph, x+1, y+1, xEnd, yEnd) + calcGValue(graph, x+1, y+1, xStart, yStart));
+				if (fCost >= maxCost.get(i) && (calcHValue(graph, x+1, y+1, xEnd, yEnd) <= maxHCost)) {
+					maxHCost = calcHValue(graph, x+1, y+1, xEnd, yEnd);
 					maxCost.set(i, fCost);
 					dir.set(i, Direction.SOUTH_EAST);
 				}
@@ -136,13 +188,18 @@ public class aStar {
 			// SOUTH-WEST
 			if (((x+1) < lenRows) && (y-1 >= 0)) {
 				double southWest = graph[x+1][y-1];
-				fCost = southWest * (calcHValue(x+1, y-1, xEnd, yEnd) + calcGValue(x+1, y-1, xStart, yStart));
-				if (fCost > maxCost.get(i)) {
+				if (x == lenRows-1 || y == 0)
+					fCost = 0;
+				else
+					fCost = southWest * (calcHValue(graph, x+1, y-1, xEnd, yEnd) + calcGValue(graph, x+1, y-1, xStart, yStart));
+				if (fCost >= maxCost.get(i) && (calcHValue(graph, x+1, y-1, xEnd, yEnd) <= maxHCost)) {
+					maxHCost = calcHValue(graph, x+1, y-1, xEnd, yEnd);
 					maxCost.set(i, fCost);
 					dir.set(i, Direction.SOUTH_WEST);
 				}
 			}
 			
+			//System.out.println(dir.get(i));
 			switch(dir.get(i)) {
 				case NORTH:
 					x--;
@@ -175,6 +232,10 @@ public class aStar {
 					break;
 			}
 			
+			System.out.println(x + " " + y);
+			System.out.println(dir.get(i));
+			
+			maxHCost = 0;
 			i++;
 			
 		}
@@ -194,11 +255,6 @@ public class aStar {
         graph[5] = new double[]{0.5, 1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5};
         graph[6] = new double[]{0.5, 1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5};
         graph[7] = new double[]{0.5, 1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5};
-	
-        Direction d = AStar(graph, 7, 1, 0, 7);
-        Direction d1 = AStar(graph, 6, 1, 0, 7); // north
-        Direction d2 = AStar(graph, 1, 1, 0, 7); // east
-        Direction d3 = AStar(graph, 2, 1, 0, 7); // northeast
-        
-   }
+		
+	}
 }
